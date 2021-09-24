dcf06f9c3a505ebe95035dde69e97f7c02068d0d
Avilla <ryanavilla998@gmail.com>
2021-09-24 16:58:26 -0300

    Salvo, preciso concluir ela

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 # importações
-from flask import Flask, jsonify # preparar resposta HTTP no formato json
+from flask import Flask, jsonify, request # preparar resposta HTTP no formato json
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 
 # configurações
 app = Flask(__name__)
+CORS(app)
 # caminho do arquivo de banco de dados
 path = os.path.dirname(os.path.abspath(__file__)) # sugestao do Kaue
 arquivobd = os.path.join(path, 'pessoas.db')
--- servidor-backend.py
+++ servidor-backend.py
@@ -1,3 +1,4 @@
+from werkzeug.datastructures import RequestCacheControl
 from config import *
 from modelo import Pessoa
 
@@ -16,5 +17,40 @@ def listar_pessoas():
     resposta  = jsonify(pessoas_em_json)
     resposta.headers.add("Access-Control-Allow-Origin", "*")
     return resposta
+
+# teste da rota: curl -d '{"nome":"James Kirk", "telefone":"92212-1212", "email":"jakirk@gmail.com"}' -X POST -H "Content-Type:application/json" localhost:5000/incluir_pessoa
+@app.route("/incluir_pessoa", methods=['post'])
+def incluir_pessoa():
+    # preparar uma resposta otimista
+    resposta = jsonify({"resultado": "ok", "detalhes": "ok"})
+    # receber as informações da nova pessoa
+    dados = request.get_json() #(force=True) dispensa Content-Type na requisição
+    try: # tentar executar a operação
+      nova = Pessoa(**dados) # criar a nova pessoa
+      db.session.add(nova) # adicionar no BD
+      db.session.commit() # efetivar a operação de gravação
+    except Exception as e: # em caso de erro...
+      # informar mensagem de erro
+      resposta = jsonify({"resultado":"erro", "detalhes":str(e)})
+    # adicionar cabeçalho de liberação de origem
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta # responder!
+
+# teste: curl -X DELETE http://localhost:5000/excluir_pessoa/1
+@app.route("/excluir_pessoa/<int:pessoa_id>", methods=['DELETE'])
+def excluir_pessoa(pessoa_id):
+    # preparar uma resposta otimista
+    resposta = jsonify({"resultado": "ok", "detalhes": "ok"})
+    try:
+        # excluir a pessoa do ID informado
+        Pessoa.query.filter(Pessoa.id == pessoa_id).delete()
+        # confirmar a exclusão
+        db.session.commit()
+    except Exception as e:
+        # informar mensagem de erro
+        resposta = jsonify({"resultado":"erro", "detalhes":str(e)})
+    # adicionar cabeçalho de liberação de origem
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta # responder!
 
 app.run(debug=True)
